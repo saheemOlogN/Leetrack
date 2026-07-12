4a79d2ae83bc9e569bbcfeac910b94c6a3a4fc9e
Saheem <saheem.nakhwa24@gmail.com>
2026-07-12 17:26:58 +0530

    Time: 2 ms (79.1%), Space: 143.4 MB (16.78%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> res(n,-1);
+        vector<long long> pref(n);
+        pref[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pref[i]=pref[i-1]+nums[i];
+        }
+
+        for(int i=k;i<n-k;i++){
+            long long temp = ((i - k > 0) ? pref[i + k] - pref[i - k - 1] :  pref[i + k]);
+
+           int avg=temp/ (2*k+1);
+           res[i]=avg;
+        }
+
+        return res;
+    }
+};
