6de7a6fbac4a445938527d23caa8018b5d4bda3d
Saheem Nakhwa <saheem.nakhwa24@gmail.com>
2026-07-31 20:35:49 +0530

    Time: 23 ms (53.95%), Space: 17.6 MB (23.83%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -18,7 +18,7 @@ public:
                     left++;
                     right--;
                     while(left<right && nums[left]==nums[left-1]) left++;
-                    while(left<right && nums[right]==nums[right-1]) right++;
+                    while(left<right && nums[right]==nums[right+1]) right--;
                     }
                     else if(sum<target){
                        left++;
