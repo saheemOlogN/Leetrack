5421bc767b40d9ba73ac697fbd4a2ab9c5e20a52
Saheem Nakhwa <saheem.nakhwa24@gmail.com>
2026-08-23 20:30:49 +0530

    Time: 7 ms (21.45%), Space: 18.7 MB (5.36%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+int leftSum(string num){
+    int res=0;
+    int i=0;
+    int j=num.size();
+    int mid=i+(j-i)/2;
+    for(int i=0;i<mid;i++){
+        if(num[i]!='?') res+=num[i]-'0';
+    }
+    return res;
+}
+
+int rightSum(string num){
+    int res=0;
+    int i=0;
+    int j=num.size();
+    int mid=i+(j-i)/2;
+    for(int i=mid;i<num.size();i++){
+        if(num[i]!='?') res+=num[i]-'0';
+    }
+    return res;
+}
+
+int leftQ(string num){
+    int res=0;
+    int i=0;
+    int j=num.size();
+    int mid=i+(j-i)/2;
+    for(int i=0;i<mid;i++){
+        if(num[i]=='?') res++;
+    }
+    return res;
+}
+
+int rightQ(string num){
+    int res=0;
+    int i=0;
+    int j=num.size();
+    int mid=i+(j-i)/2;
+    for(int i=mid;i<num.size();i++){
+        if(num[i]=='?') res++;
+    }
+    return res;
+}
+
+    bool sumGame(string num) {
+        int ls=leftSum(num);
+        int rs=rightSum(num);
+        int lq=leftQ(num);
+        int rq=rightQ(num);
+        if((lq+rq)%2!=0) return true;
+
+        int eq1 = 2*ls+9*lq;       
+        int eq2 = 2*rs+9*rq;   
+        if(eq1==eq2) return false;
+        return true;   
+    }
+};
