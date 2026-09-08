263c46061a6f0e5406e452fd385805c116ec989f
Saheem Nakhwa <saheem.nakhwa24@gmail.com>
2026-09-08 10:52:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
-int count(int n){
-    if(n<3) return 0;
-    return (n-1)/3;
-}
     int countCommas(int n) {
-        int res=0;
-        
-        for(int i=1000;i<=n;i++){
-            string temp=to_string(i);
-            res+=count(temp.size());
-        }
-
-
-        return res;
+        if (n < 1000) return 0;
+        return (n - (1000-1));
     }
 };
